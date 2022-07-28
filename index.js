0801d43eb4a4e8bdfa2c883699f13cb96f5a9328
Jason Miclat <jason.miclat@gmail.com>
2022-07-27 21:59:26 -0600

    changed port number

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ app.use('/api/v1/jobs',authenticateUser, jobsRouter)
 app.use(notFoundMiddleware)
 app.use(errorHandleMiddleware)
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT;
 
 const start  = () => {
 
